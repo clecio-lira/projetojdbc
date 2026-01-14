62f63e84a237d790a1435682ebafb14a39c83e4d
Clécio de Lira Alves <cclecioc.com@gmail.com>
2026-01-14 08:18:36 -0300

    department entity class

--- src/main/java/com/projetojdbc/Main.java
+++ src/main/java/com/projetojdbc/Main.java
@@ -1,7 +1,10 @@
 package com.projetojdbc;
 
+import com.projetojdbc.entities.Department;
+
 public class Main {
     static void main() {
-
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
