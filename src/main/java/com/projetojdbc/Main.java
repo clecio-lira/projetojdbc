dc05f8871b25019ba85dab669e63e85cb10e31ab
Clécio de Lira Alves <cclecioc.com@gmail.com>
2026-01-14 08:25:54 -0300

    seller entity class

--- src/main/java/com/projetojdbc/Main.java
+++ src/main/java/com/projetojdbc/Main.java
@@ -1,10 +1,16 @@
 package com.projetojdbc;
 
 import com.projetojdbc.entities.Department;
+import com.projetojdbc.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
-    static void main() {
+    static void main(String[] args) {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        Seller obj1 = new Seller(1, "clecio", "clecio@email.com", new Date(), 3000.00, obj);
+        System.out.println(obj1);
     }
 }
