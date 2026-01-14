88673deb488ce4d304e16b1984355c548fd2fac0
Clécio de Lira Alves <cclecioc.com@gmail.com>
2026-01-14 14:49:16 -0300

    findbydepartment implementation

--- src/main/java/com/projetojdbc/Main.java
+++ src/main/java/com/projetojdbc/Main.java
@@ -6,6 +6,7 @@ import com.projetojdbc.model.dao.DaoFactory;
 import com.projetojdbc.model.dao.SellerDao;
 
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     static void main(String[] args) {
@@ -13,7 +14,13 @@ public class Main {
 
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDerpartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj: list) {
+            System.out.println(obj);
+        }
     }
 }
